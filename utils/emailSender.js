836cf6ad6d1dd80ee708502860947a700c149b7c
MauriVargas17 <vargasmauricio65@gmail.com>
2023-06-10 00:25:10 -0400

    make it better

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -6,24 +6,32 @@ dotenv.config({ path: './config.env' });
 const sendEmail = async (options) => {
   let transporter;
 
-  if (process.env.NODE_ENV === 'production') {
-    transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_ADDRESS,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-    });
-  } else {
-    transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST_TEST,
-      port: process.env.EMAIL_PORT_TEST,
-      auth: {
-        user: process.env.EMAIL_ADDRESS_TEST,
-        pass: process.env.EMAIL_PASSWORD_TEST,
-      },
-    });
-  }
+  transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // if (process.env.NODE_ENV === 'production') {
+  //   transporter = nodemailer.createTransport({
+  //     service: 'gmail',
+  //     auth: {
+  //       user: process.env.EMAIL_ADDRESS,
+  //       pass: process.env.EMAIL_PASSWORD,
+  //     },
+  //   });
+  // } else {
+  //   transporter = nodemailer.createTransport({
+  //     host: process.env.EMAIL_HOST_TEST,
+  //     port: process.env.EMAIL_PORT_TEST,
+  //     auth: {
+  //       user: process.env.EMAIL_ADDRESS_TEST,
+  //       pass: process.env.EMAIL_PASSWORD_TEST,
+  //     },
+  //   });
+  // }
 
   const mailOptions = {
     from: 'Fotocopias UPB <fotocopias.upblpz@gmail.com>',
